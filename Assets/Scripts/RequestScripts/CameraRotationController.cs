9e774770007943c4afcc9fecf718cd6ed0b3caee
ASRAKAN <muratsahinkilic1997@gmail.com>
2020-11-26 21:29:25 +0300

    +feature : camera rotation

---
+++ Assets/Scripts/RequestScripts/CameraRotationController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons.Requests
+{
+    public class CameraRotationController : MonoBehaviour
+    {
+        [SerializeField] private Transform _targetTransform;
+        [SerializeField] private Transform _cameraTransform;
+        [SerializeField] private float _lerpSpeed = 3;
+        private void Update()
+        {
+            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, Quaternion.LookRotation(_targetTransform.position- _cameraTransform.position), Time.deltaTime * _lerpSpeed);
+        }
+    }
+}

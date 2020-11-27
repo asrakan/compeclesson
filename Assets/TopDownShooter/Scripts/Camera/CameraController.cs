d933cbf9a13974fcea2e334d5cc30ae7944198d2
ASRAKAN <muratsahinkilic1997@gmail.com>
2020-11-27 21:06:52 +0300

    +added : basic movement for camera

---
+++ Assets/TopDownShooter/Scripts/Camera/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CameraSettings _cameraSettings;
+        [SerializeField] private Transform _targetTransform;
+        [SerializeField] private Transform _cameraTransform;
+
+        private void Update()
+        {
+            CameraRotationFollow();
+            CameraMovementFollow();
+        }
+
+        private void CameraRotationFollow()
+        {
+            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, Quaternion.LookRotation(_targetTransform.position - _cameraTransform.position), Time.deltaTime * _cameraSettings.RotationLerpSpeed);
+        }
+
+
+        private void CameraMovementFollow()
+        {
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _targetTransform.position + _cameraSettings.PositionOffset, Time.deltaTime * _cameraSettings.PositionLerp);
+        }
+
+    }
+}
---
+++ Assets/TopDownShooter/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    [CreateAssetMenu(menuName = "Topdown Shooter/Camera/Camera Settings")]
+    public class CameraSettings : ScriptableObject
+    {
+        [Header("Rotation")]
+        [SerializeField] private float _rotationLerpSpeed = 1;
+        public float RotationLerpSpeed { get { return _rotationLerpSpeed; } }
+
+
+        [Header("Position")]
+        [SerializeField] private Vector3 _positionOffset;
+        public Vector3 PositionOffset { get { return _positionOffset; } }
+
+        [SerializeField] private float _positionLerp = 1;
+        public float PositionLerp { get { return _positionLerp; } }
+    }
+}

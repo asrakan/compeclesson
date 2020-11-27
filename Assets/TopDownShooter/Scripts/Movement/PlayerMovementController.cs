15ce224269e4e471c4072002b1b731e448fb87d8
ASRAKAN <muratsahinkilic1997@gmail.com>
2020-11-27 21:28:57 +0300

    +created : basic movement system

---
+++ Assets/TopDownShooter/Scripts/Movement/PlayerMovementController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TopDownShooter.PlayerInput;
+using UnityEngine;
+
+
+namespace TopDownShooter.PlayerMovement
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private InputData _inputData;
+        [SerializeField] private PlayerMovementSettings _playerMovementSettings;
+        private void Update()
+        {
+            _rigidbody.MovePosition(_rigidbody.position + (_rigidbody.transform.forward * _inputData.Vertical * _playerMovementSettings.VerticalSpeed));
+
+            _rigidbody.MovePosition(_rigidbody.position + (_rigidbody.transform.right * _inputData.Horizontal * _playerMovementSettings.HorizontalSpeed));
+        }
+    }
+}
---
+++ Assets/TopDownShooter/Scripts/Movement/PlayerMovementSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerMovement
+{
+    [CreateAssetMenu(menuName = "Topdown Shooter/Player/Movement Settings")]
+    public class PlayerMovementSettings : ScriptableObject
+    {
+        public float HorizontalSpeed = 5;
+        public float VerticalSpeed = 5;
+    }
+}

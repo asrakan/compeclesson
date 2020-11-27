d9adfee1fcd4438a626167416351f1923837417a
ASRAKAN <muratsahinkilic1997@gmail.com>
2020-11-27 20:37:55 +0300

    +basic phsyics

---
+++ Assets/Scripts/Lessons/ControlRigidbody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    public class ControlRigidbody : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private ControlRigidbodySettings _settings;
+        private void Update()
+        {
+            bool spaceKeyDown = Input.GetKeyDown(KeyCode.Space);
+            if (spaceKeyDown)
+            {
+                _rigidbody.AddForce(_settings.JumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Lessons/ControlRigidbodySettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    [CreateAssetMenu(menuName = "Lessons/Lesson1/Control Rigidbody Settings Settings")]
+    public class ControlRigidbodySettings : ScriptableObject
+    {
+        [SerializeField] private Vector3 _jumpForce;
+        public Vector3 JumpForce { get { return _jumpForce; } }
+    }
+}

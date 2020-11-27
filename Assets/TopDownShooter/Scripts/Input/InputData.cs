36d6fefaec745f1370226d4ff7a24d02a461de9e
ASRAKAN <muratsahinkilic1997@gmail.com>
2020-11-27 21:28:45 +0300

    +created : basic input systme

---
+++ Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TopDownShooter.PlayerInput
+{
+    [CreateAssetMenu(menuName = "Topdown Shooter/Input/Input Data")]
+    public class InputData : ScriptableObject
+    {
+        public float Horizontal;
+        public float Vertical;
+    }
+}
---
+++ Assets/TopDownShooter/Scripts/Input/InputManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerInput
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private InputData _inputData;
+        private void Update()
+        {
+            _inputData.Horizontal = Input.GetAxis("Horizontal");
+            _inputData.Vertical = Input.GetAxis("Vertical");
+        }
+    }
+}

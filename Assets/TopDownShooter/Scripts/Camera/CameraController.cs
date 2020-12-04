fbf56c333d68d33014f4d32ce2f5625b9ba4afa7
ASRAKAN <muratsahinkilic1997@gmail.com>
2020-12-04 21:24:31 +0300

    +camera controller

--- Assets/TopDownShooter/Scripts/Camera/CameraController.cs
+++ Assets/TopDownShooter/Scripts/Camera/CameraController.cs
@@ -7,7 +7,8 @@ namespace TopDownShooter.Camera
     public class CameraController : MonoBehaviour
     {
         [SerializeField] private CameraSettings _cameraSettings;
-        [SerializeField] private Transform _targetTransform;
+        [SerializeField] private Transform _rotationTarget;
+        [SerializeField] private Transform _positionTarget;
         [SerializeField] private Transform _cameraTransform;
 
         private void Update()
@@ -18,14 +19,13 @@ namespace TopDownShooter.Camera
 
         private void CameraRotationFollow()
         {
-            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, Quaternion.LookRotation(_targetTransform.forward), Time.deltaTime * _cameraSettings.RotationLerpSpeed);
+            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, Quaternion.LookRotation(_rotationTarget.forward), Time.deltaTime * _cameraSettings.RotationLerpSpeed);
         }
 
 
         private void CameraMovementFollow()
         {
-            Vector3 offset = (_cameraTransform.right * _cameraSettings.PositionOffset.x)+ ( _cameraTransform.up * _cameraSettings.PositionOffset.y)+(_cameraTransform.forward * _cameraSettings.PositionOffset.z);
-            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _targetTransform.position + offset, Time.deltaTime * _cameraSettings.PositionLerp);
+            _cameraTransform.localPosition = _cameraSettings.PositionOffset;
         }
 
     }

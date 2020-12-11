bc8805b73a77f57904af56c733093be735c81df1
ASRAKAN <muratsahinkilic1997@gmail.com>
2020-12-11 21:13:04 +0300

    +create : shoot manager

---
+++ Assets/TopDownShooter/Scripts/Inventory/ScriptableShootManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TopDownShooter.Inventory
+{
+    [CreateAssetMenu(menuName = "Topdown Shooter/Inventory/ScriptableShootManager")]
+    public class ScriptableShootManager : AbstractScriptableManager<ScriptableShootManager>
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Debug.Log("scriptable shoot manager instantiated");
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+            Debug.Log("scriptable shoot manager destroyed");
+        }
+
+        public void Shoot()
+        {
+            
+        }
+    }
+}

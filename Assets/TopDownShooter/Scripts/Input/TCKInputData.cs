ceedffeca6c791a72861739c34c6dfa3fc6d5371
ASRAKAN <muratsahinkilic1997@gmail.com>
2020-12-24 21:03:35 +0300

    +tck input module create

---
+++ Assets/TopDownShooter/Scripts/Input/TCKInputData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TouchControlsKit;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerInput
+{
+    [CreateAssetMenu(menuName = "Topdown Shooter/Input/TCK Input Data")]
+    public class TCKInputData : AbstractInputData
+    {
+        public string AxisName;
+        public bool IsAction;
+        public override void ProcessInput()
+        {
+            if (IsAction)
+            {
+                if (TCKInput.GetAction(AxisName, EActionEvent.Down))
+                {
+                    Horizontal = 1;
+                }
+                else if (TCKInput.GetAction(AxisName, EActionEvent.Up))
+                {
+                    Horizontal = 0;
+                }
+            }
+            else
+            {
+                Vector2 move = TCKInput.GetAxis(AxisName); // NEW func since ver 1.5.5
+                Horizontal = move.x;
+                Vertical = move.y;
+            }
+        }
+    }
+}
